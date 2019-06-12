a4ef60a1443a62edd9bc5b2b6200bd0d4bcaa60d
Mohammed Saqib <saqibm128@gmail.com>
2019-06-12 16:37:08 -0400

    added py doc strings

--- util_funcs.py
+++ util_funcs.py
@@ -9,9 +9,20 @@ import pymongo
 import itertools
 
 def get_abs_files(root_dir_path):
-    """
-    helper func to return full path names. helps with nested structure of
+    """helper func to return full path names. helps with nested structure of
         extracted files
+
+    Parameters
+    ----------
+    root_dir_path : type
+        Description of parameter `root_dir_path`.
+
+    Returns
+    -------
+    list
+        Full paths of files, including directories, inside the root_dir_path
+        If root_dir_path is a file and not a directory, this will fail
+
     """
     subdirs = os.listdir(root_dir_path)
     subdirs = [path.join(root_dir_path, subdir) for subdir in subdirs]
@@ -20,10 +31,19 @@ def get_abs_files(root_dir_path):
 
 
 def get_annotation_types():
-    """
-    Used to get the specific annotation types. These are specified in
-        .tse files and label specific time subsequences of the entire record
-        These are also used in .lbl files to label time sequences of single channels
+    """Used to get the specific annotation types. These are specified in
+            .tse files and label specific time subsequences of the entire record
+            These are also used in .lbl files to label time sequences of single channels
+
+    Parameters
+    ----------
+
+
+    Returns
+    -------
+    list
+        list of the lower case annotation codes
+
     """
     #https://www.isip.piconepress.com/projects/tuh_eeg/downloads/tuh_eeg_seizure/v1.5.0/_DOCS/
     return pd.read_csv(
@@ -37,13 +57,39 @@ def get_data_split():
     return ["train", "dev_test"]
 
 def get_reference_node_types():
-    """
-    The TUH dataset is further split based on what was the reference voltage
+    """The TUH dataset is further split based on what was the reference voltage
         See: https://www.isip.piconepress.com/publications/conference_proceedings/2016/ieee_spmb/montages/
+
+    Parameters
+    ----------
+
+
+    Returns
+    -------
+    list
+        strings representing the appropriate subdirectory that describes
+        reference
     """
     return ["01_tcp_ar", "02_tcp_le", "03_tcp_ar_a"]
 
 def get_patient_dir_names(data_split, ref, full_path=True):
+    """ Gets the path names of the folders holding the patient info
+
+    Parameters
+    ----------
+    data_split : str
+        Which data split the patient belongs to
+    ref : str
+        which reference voltage system that is used
+    full_path : bool
+        Whether or not to return abs path
+
+    Returns
+    -------
+    list
+        list of strings describing path
+
+    """
     assert data_split in get_data_split()
     assert ref in get_reference_node_types()
     p = Pool()
@@ -58,6 +104,23 @@ def get_patient_dir_names(data_split, ref, full_path=True):
         return [path.basename(patient_dir) for patient_dir in patient_dirs]
 
 def get_session_dir_names(data_split, ref, full_path=True, patient_dirs=None):
+    """Gets the path names of the folders holding the session info
+        (patients can have multiple eeg sessions)
+
+    Parameters
+    ----------
+    data_split : str
+        Which data split the patient belongs to
+    ref : str
+        which reference voltage system that is used
+    full_path : bool
+        Whether or not to return abs path
+
+    Returns
+    -------
+    list
+        list of strings describing path
+    """
     assert data_split in get_data_split()
     assert ref in get_reference_node_types()
     p = Pool()
