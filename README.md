68ae2334e18118b9ed8cabcd10b9e6a841c0da43
Mohammed Saqib <saqibm128@gmail.com>
2019-06-12 16:31:15 -0400

    created more helper funcs to read in data, updated the readme to reflect changes to config.json, importing in a csv of annotation labels

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # dbmi_eeg_clustering
+Uses the TUH dataset:  https://www.isip.piconepress.com/projects/tuh_eeg/
+
+This dataset is segmented into a train set and test set and includes annotations.
+In addition, there are subsets of the data based on the exact reference node of the EEG.
+
+# Setup
+Reads the directory of the data using config.json file
+
+``` config.json
+{
+  "train_01_tcp_ar": "/mnt/c/Users/sawer/src/dbmi/tuh/v1.5.0/edf/train/01_tcp_ar/",
+  "dev_test_01_tcp_ar": "/mnt/c/Users/sawer/src/dbmi/tuh/v1.5.0/edf/dev_test/01_tcp_ar/",
+  "train_02_tcp_le": "/mnt/c/Users/sawer/src/dbmi/tuh/v1.5.0/edf/train/02_tcp_le/",
+  "dev_test_02_tcp_le": "/mnt/c/Users/sawer/src/dbmi/tuh/v1.5.0/edf/dev_test/02_tcp_le/",
+  "train_03_tcp_ar_a": "/mnt/c/Users/sawer/src/dbmi/tuh/v1.5.0/edf/dev_test/03_tcp_ar_a/",
+  "dev_test_03_tcp_ar_a": "/mnt/c/Users/sawer/src/dbmi/tuh/v1.5.0/edf/train/03_tcp_ar_a/"
+}
+
+```
--- util_funcs.py
+++ util_funcs.py
@@ -1,14 +1,78 @@
 import pickle as pkl
 import json, os
+import os.path as path
 from addict import Dict
 from pathos.multiprocessing import Pool
 import pandas as pd
 import numpy as np
 import pymongo
+import itertools
 
-def get_seizure_types():
-    return ['ABSZ', 'CPSZ', 'FNSZ', 'GNSZ', 'MYSZ', 'SPSZ', 'TCSZ', 'TNSZ']
+def get_abs_files(root_dir_path):
+    """
+    helper func to return full path names. helps with nested structure of
+        extracted files
+    """
+    subdirs = os.listdir(root_dir_path)
+    subdirs = [path.join(root_dir_path, subdir) for subdir in subdirs]
+    return subdirs
+
+
+
+def get_annotation_types():
+    """
+    Used to get the specific annotation types. These are specified in
+        .tse files and label specific time subsequences of the entire record
+        These are also used in .lbl files to label time sequences of single channels
+    """
+    #https://www.isip.piconepress.com/projects/tuh_eeg/downloads/tuh_eeg_seizure/v1.5.0/_DOCS/
+    return pd.read_csv(
+        "data_labels.csv",
+         header=0,
+         dtype=str,
+         keep_default_na=False,
+         )["class_code"].str.lower().tolist()
+
+def get_data_split():
+    return ["train", "dev_test"]
+
+def get_reference_node_types():
+    """
+    The TUH dataset is further split based on what was the reference voltage
+        See: https://www.isip.piconepress.com/publications/conference_proceedings/2016/ieee_spmb/montages/
+    """
+    return ["01_tcp_ar", "02_tcp_le", "03_tcp_ar_a"]
+
+def get_patient_dir_names(data_split, ref, full_path=True):
+    assert data_split in get_data_split()
+    assert ref in get_reference_node_types()
+    p = Pool()
+    root_dir_entry = data_split + "_" + ref
+    config = read_config()
+    root_dir_path = config[root_dir_entry]
+    subdirs = get_abs_files(root_dir_path)
+    patient_dirs = list(itertools.chain.from_iterable(p.map(get_abs_files, subdirs)))
+    if full_path:
+        return patient_dirs
+    else:
+        return [path.basename(patient_dir) for patient_dir in patient_dirs]
+
+def get_session_dir_names(data_split, ref, full_path=True, patient_dirs=None):
+    assert data_split in get_data_split()
+    assert ref in get_reference_node_types()
+    p = Pool()
+    if patient_dirs is None:
+        patient_dirs = get_patient_dir_names(data_split, ref)
+    session_dirs = list(itertools.chain.from_iterable(p.map(get_abs_files, patient_dirs)))
+    if full_path:
+        return session_dirs
+    else:
+        return [path.basename(session_dir) for session_dir in session_dirs]
+
 def get_mongo_client(path = "config.json"):
+    '''
+    Used for Sacred to record results
+    '''
     config = read_config(path)
     if "mongo_uri" not in config.keys():
         return pymongo.MongoClient()
@@ -19,13 +83,8 @@ def get_mongo_client(path = "config.json"):
 def read_config(path="config.json"):
     return json.load(open(path, "rb"))
 
-def read_preproc_1(id):
-    root_path = read_config()["preprocessed_1"]
-    return pkl.load(open(os.path.join(root_path, "seiz_{}.pkl".format(id)), "rb"));
-
 
 if __name__ == "__main__":
     print(read_config())
-    print(read_preproc_1(1))
-    print(read_preproc_2(1))
-    print(read_all_into_df(num_files=100))
+    print(get_annotation_types())
+    print('spsw' in get_annotation_types())
