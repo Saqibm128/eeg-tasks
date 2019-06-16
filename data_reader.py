b0c293e80a33922cda3bdc1f915fb98f1abc428b
Mohammed Saqib <saqibm128@gmail.com>
2019-06-16 11:57:40 -0400

    Refactored code to move read functionality into its own file

---
+++ data_reader.py
@@ -0,0 +1,174 @@
+import pandas as pd
+import numpy as np
+import itertools
+import pyedflib
+from os import path
+from pathos.multiprocessing import Pool
+from util_funcs import read_config, get_abs_files, get_annotation_types, get_data_split, get_reference_node_types
+
+
+# dataset = Dataset(num_files=10)
+# eeg_df_256_hz, labels_by_256_hz = Dataset().get(i)
+# model
+
+
+#TODO: no way we can load all the data at once
+# https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
+
+
+def read_tse_file(path):
+    tse_data_lines = []
+    with open(path, 'r') as f:
+        for line in f:
+            if "#" in line:
+                continue #this is a comment
+            elif "version" in line:
+                assert "tse_v1.0.0" in line #assert file is correct version
+            elif len(line.strip()) == 0:
+                continue #Just blank space, continue
+            else:
+                line = line.strip()
+                subparts = line.split()
+                tse_data_line = pd.Series(index=['start', 'end', 'label', 'p'])
+                tse_data_line['start'] = float(subparts[0])
+                tse_data_line['end'] = float(subparts[1])
+                tse_data_line['label'] = str(subparts[2])
+                tse_data_line['p'] = float(subparts[3])
+                tse_data_lines.append(tse_data_line)
+    tse_data = pd.concat(tse_data_lines, axis=1).T
+    return tse_data
+
+def convert_edf_path_to_tse(edf_path):
+    return edf_path[:-4] + ".tse"
+
+def read_tse_file_and_return_ts(tse_path, ts_index):
+    ann_y = read_tse_file(tse_path)
+    ann_y_t = pd.DataFrame(columns=util_funcs.get_annotation_types(), index=ts_index)
+    ann_y.apply(lambda row: ann_y_t[row['label']].loc[pd.Timedelta(seconds=row['start']):pd.Timedelta(seconds=row['end'])].fillna(row['p'], inplace=True), axis=1)
+    ann_y_t.fillna(0, inplace=True)
+    return ann_y_t
+
+
+def edf_eeg_2_df(path, resample=None):
+    """ Transforms from EDF to pd.df, with channel labels as columns.
+        This does not attempt to concatenate multiple time series but only takes
+        a single edf filepath
+
+    Parameters
+    ----------
+    path : str
+        path of the edf file
+
+    resample : pd.Timedelta
+        if None, returns original data with original sampling
+        otherwise, resamples to correct Timedelta using forward filling
+
+    Returns
+    -------
+    pd.DataFrame
+        index is time, columns is waveform channel label
+
+    """
+    reader = pyedflib.EdfReader(path)
+    channel_names = [headerDict['label'] for headerDict in reader.getSignalHeaders()]
+    sample_rates = [headerDict['sample_rate'] for headerDict in reader.getSignalHeaders()]
+    start_time = reader.getStartdatetime()
+    all_channels = []
+    for i, channel_name in enumerate(channel_names):
+        signal_data = reader.readSignal(i)
+        signal_data = pd.Series(
+            signal_data,
+            index=pd.date_range(
+                start=start_time,
+                freq=pd.Timedelta(seconds=1/sample_rates[i]),
+                periods=len(signal_data)
+                ),
+            name=channel_name
+            )
+        all_channels.append(signal_data)
+    return pd.concat(all_channels, axis=1)
+
+def get_patient_dir_names(data_split, ref, full_path=True):
+    """ Gets the path names of the folders holding the patient info
+
+    Parameters
+    ----------
+    data_split : str
+        Which data split the patient belongs to
+    ref : str
+        which reference voltage system that is used
+    full_path : bool
+        Whether or not to return abs path
+
+    Returns
+    -------
+    list
+        list of strings describing path
+
+    """
+    assert data_split in get_data_split()
+    assert ref in get_reference_node_types()
+    p = Pool()
+    root_dir_entry = data_split + "_" + ref
+    config = read_config()
+    root_dir_path = config[root_dir_entry]
+    subdirs = get_abs_files(root_dir_path)
+    patient_dirs = list(itertools.chain.from_iterable(p.map(get_abs_files, subdirs)))
+    if full_path:
+        return patient_dirs
+    else:
+        return [path.basename(patient_dir) for patient_dir in patient_dirs]
+
+def get_session_dir_names(data_split, ref, full_path=True, patient_dirs=None):
+    """Gets the path names of the folders holding the session info
+        (patients can have multiple eeg sessions)
+
+    Parameters
+    ----------
+    data_split : str
+        Which data split the patient belongs to
+    ref : str
+        which reference voltage system that is used
+    full_path : bool
+        Whether or not to return abs path
+
+    Returns
+    -------
+    list
+        list of strings describing path
+    """
+    assert data_split in get_data_split()
+    assert ref in get_reference_node_types()
+    p = Pool()
+    if patient_dirs is None:
+        patient_dirs = get_patient_dir_names(data_split, ref)
+    session_dirs = list(itertools.chain.from_iterable(p.map(get_abs_files, patient_dirs)))
+    if full_path:
+        return session_dirs
+    else:
+        return [path.basename(session_dir) for session_dir in session_dirs]
+
+def get_all_token_file_names(data_split, ref, full_path=True):
+    p = Pool()
+    session_dirs = get_session_dir_names(data_split, ref)
+    token_fns = list(itertools.chain.from_iterable(p.map(get_token_file_names, session_dirs)))
+    if full_path:
+        return token_fns
+    else:
+        return [path.basename(token_fn) for token_fn in token_fns]
+
+
+def get_token_file_names(session_dir_path, full_path=True):
+    sess_file_names = get_abs_files(session_dir_path)
+    time_series_fns = [fn for fn in sess_file_names if fn[-4:] == '.edf']
+    if full_path:
+        return time_series_fns
+    else:
+        return [path.basename(fn) for fn in time_series_fns]
+
+def get_session_data(session_dir_path):
+    time_series_fns = get_token_file_names(session_dir_path)
+    signal_dfs = []
+    for fn in time_series_fns:
+        signal_dfs.append(edf_eeg_2_df(fn))
+    return pd.concat(signal_dfs)
--- util_funcs.py
+++ util_funcs.py
@@ -2,76 +2,11 @@ import pickle as pkl
 import json, os
 import os.path as path
 from addict import Dict
-from pathos.multiprocessing import Pool
 import pandas as pd
 import numpy as np
 import pymongo
 import itertools
 import pyedflib
-
-def read_tse_file(path):
-    tse_data_lines = []
-    with open(path, 'r') as f:
-        for line in f:
-            if "#" in line:
-                continue #this is a comment
-            elif "version" in line:
-                assert "tse_v1.0.0" in line #assert file is correct version
-            elif len(line.strip()) == 0:
-                continue #Just blank space, continue
-            else:
-                line = line.strip()
-                subparts = line.split()
-                tse_data_line = pd.Series(index=['start', 'end', 'label', 'p'])
-                tse_data_line['start'] = float(subparts[0])
-                tse_data_line['end'] = float(subparts[1])
-                tse_data_line['label'] = str(subparts[2])
-                tse_data_line['p'] = float(subparts[3])
-                tse_data_lines.append(tse_data_line)
-    tse_data = pd.concat(tse_data_lines, axis=1).T
-    return tse_data
-
-def read_tse_file_and_return_ts(path):
-    pass #Not sure if I need this yet
-    tse_data = read_tse_file(path)
-    ann_types = get_annotation_types()
-
-
-def edf_eeg_2_df(path):
-    """ Transforms from EDF to pd.df, with channel labels as columns.
-        This does not attempt to concatenate multiple time series but only takes
-        a single edf filepath
-
-    Parameters
-    ----------
-    path : str
-        path of the edf file
-
-    Returns
-    -------
-    pd.DataFrame
-        index is time, columns is waveform channel label
-
-    """
-    reader = pyedflib.EdfReader(path)
-    channel_names = [headerDict['label'] for headerDict in reader.getSignalHeaders()]
-    sample_rates = [headerDict['sample_rate'] for headerDict in reader.getSignalHeaders()]
-    start_time = reader.getStartdatetime()
-    all_channels = []
-    for i, channel_name in enumerate(channel_names):
-        signal_data = reader.readSignal(i)
-        signal_data = pd.Series(
-            signal_data,
-            index=pd.date_range(
-                start=start_time,
-                freq=pd.Timedelta(seconds=1/sample_rates[i]),
-                periods=len(signal_data)
-                ),
-            name=channel_name
-            )
-        all_channels.append(signal_data)
-    return pd.concat(all_channels, axis=1)
-
 
 def get_abs_files(root_dir_path):
     """helper func to return full path names. helps with nested structure of
@@ -144,90 +79,6 @@ def get_reference_node_types():
     """
     return ["01_tcp_ar", "02_tcp_le", "03_tcp_ar_a"]
 
-def get_patient_dir_names(data_split, ref, full_path=True):
-    """ Gets the path names of the folders holding the patient info
-
-    Parameters
-    ----------
-    data_split : str
-        Which data split the patient belongs to
-    ref : str
-        which reference voltage system that is used
-    full_path : bool
-        Whether or not to return abs path
-
-    Returns
-    -------
-    list
-        list of strings describing path
-
-    """
-    assert data_split in get_data_split()
-    assert ref in get_reference_node_types()
-    p = Pool()
-    root_dir_entry = data_split + "_" + ref
-    config = read_config()
-    root_dir_path = config[root_dir_entry]
-    subdirs = get_abs_files(root_dir_path)
-    patient_dirs = list(itertools.chain.from_iterable(p.map(get_abs_files, subdirs)))
-    if full_path:
-        return patient_dirs
-    else:
-        return [path.basename(patient_dir) for patient_dir in patient_dirs]
-
-def get_session_dir_names(data_split, ref, full_path=True, patient_dirs=None):
-    """Gets the path names of the folders holding the session info
-        (patients can have multiple eeg sessions)
-
-    Parameters
-    ----------
-    data_split : str
-        Which data split the patient belongs to
-    ref : str
-        which reference voltage system that is used
-    full_path : bool
-        Whether or not to return abs path
-
-    Returns
-    -------
-    list
-        list of strings describing path
-    """
-    assert data_split in get_data_split()
-    assert ref in get_reference_node_types()
-    p = Pool()
-    if patient_dirs is None:
-        patient_dirs = get_patient_dir_names(data_split, ref)
-    session_dirs = list(itertools.chain.from_iterable(p.map(get_abs_files, patient_dirs)))
-    if full_path:
-        return session_dirs
-    else:
-        return [path.basename(session_dir) for session_dir in session_dirs]
-
-def get_all_token_file_names(data_split, ref, full_path=True):
-    p = Pool()
-    session_dirs = get_session_dir_names(data_split, ref)
-    token_fns = list(itertools.chain.from_iterable(p.map(get_token_file_names, session_dirs)))
-    if full_path:
-        return token_fns
-    else:
-        return [path.basename(token_fn) for token_fn in token_fns]
-
-
-def get_token_file_names(session_dir_path, full_path=True):
-    sess_file_names = get_abs_files(session_dir_path)
-    time_series_fns = [fn for fn in sess_file_names if fn[-4:] == '.edf']
-    if full_path:
-        return time_series_fns
-    else:
-        return [path.basename(fn) for fn in time_series_fns]
-
-def get_session_data(session_dir_path):
-    time_series_fns = get_token_file_names(session_dir_path)
-    signal_dfs = []
-    for fn in time_series_fns:
-        signal_dfs.append(edf_eeg_2_df(fn))
-    return pd.concat(signal_dfs)
 
 def get_mongo_client(path = "config.json"):
     '''
