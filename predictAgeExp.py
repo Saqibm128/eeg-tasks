f55ad0b0f2159f1a4661c4ecaa01b74b31a53ae6
Mohammed Saqib <saqibm128@gmail.com>
2019-07-03 11:50:19 -0400

    fixed bugs in predictAgeExp

--- predictAgeExp.py
+++ predictAgeExp.py
@@ -1,10 +1,14 @@
 import sacred
 ex = sacred.Experiment(name="age_learning_exp")
 
+import tensorflow as tf
+tf.logging.set_verbosity(tf.logging.ERROR)
+
 from keras.models import Sequential
 from keras.layers import Input, LSTM, Dense, Activation, Dropout, Masking
 from keras import optimizers
 from keras.preprocessing.sequence import pad_sequences
+from keras.callbacks import EarlyStopping
 
 #Sanity check to see if we can do something fundamental like this
 
@@ -13,7 +17,7 @@ import util_funcs
 import pandas as pd
 import numpy as np
 from os import path
-from sklearn.metrics import f1_score, make_scorer, mean_squared_error, r2_score
+from sklearn.metrics import f1_score, make_scorer, mean_squared_error, r2_score, accuracy_score
 from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.preprocessing import KBinsDiscretizer, StandardScaler
 from sklearn.decomposition import PCA
@@ -53,6 +57,8 @@ def use_lstm():
     use_simple_lstm = True
     kbins_encoding = "onehot-dense"
     window = 5
+    early_stopping = True
+    patience = 10
     input_shape = (None, None, (len(read.EdfFFTDatasetTransformer.freq_bins) - 1) * len(util_funcs.get_common_channel_names())) #variable batch, variable time steps, but constant num features
 
 @ex.named_config
@@ -66,6 +72,8 @@ def config():
     ref = "01_tcp_ar"
     train_split = "train"
     test_split = "dev_test"
+    use_dwt = False
+    dwt_max_coef = 100
     n_process = 6
     output_size = 10
     precache = True
@@ -88,8 +96,10 @@ def config():
     latent_shape = (200)
     filter = True
     discretize_age = False
+    early_stopping = False
+    patience = 2
     num_pca_comp = 10
-    lr = 0.001
+    lr = 0.01
     mask_value = -1000.0
 
 @ex.capture
@@ -105,8 +115,18 @@ def get_lstm(input_shape, latent_shape, output_size, lr, mask_value):
     sgd = optimizers.SGD(lr=lr, clipnorm=1.)
     model.compile(optimizer=sgd,
               loss='mean_squared_error',
-              metrics=['mean_squared_error'])
+              metrics=['mean_squared_error'],
+              )
     return model
+
+@ex.capture
+def get_early_stopping(early_stopping, patience):
+    if early_stopping:
+        return [
+            EarlyStopping(patience=patience),
+        ]
+    else:
+        return []
 
 @ex.capture
 def three_dim_pad(data, mask_value):
@@ -124,7 +144,7 @@ def three_dim_pad(data, mask_value):
 
 
 @ex.capture
-def get_data(split, ref, n_process, precache, num_files, window, non_overlapping, return_mode, filter):
+def get_data(split, ref, n_process, precache, num_files, window, non_overlapping, return_mode, filter, use_multiple_tokens_per_session=False):
     window = window * pd.Timedelta(seconds=1)
     if return_mode=="age":
         ageData = read.getAgesAndFileNames(split, ref)
@@ -209,6 +229,7 @@ def main(
         return_dict['kbins'] = kbins.bin_edges_
         testAges = np.array(testAges).reshape(-1, 1)
         testAges = kbins.transform(testAges)
+        print("KBins used!  Edges are: {}".format(kbins.bin_edges_))
 
     if use_simple_lstm:
         ageScaler = StandardScaler()
@@ -218,7 +239,11 @@ def main(
         testAges = ageScaler.transform(testAges)
         model = get_lstm()
         x = pad_sequences(data)
-        model.fit(x, ages, epochs=num_epochs, validation_split=validation_size)
+        model.fit(x,
+                  ages,
+                  epochs=num_epochs,
+                  validation_split=validation_size,
+                  callbacks=get_early_stopping())
         testX = pad_sequences(testData)
         score = model.evaluate(testX, testAges)
         y_pred = model.predict(testX)
@@ -249,7 +274,7 @@ def main(
             steps = steps[1:]
         p = Pipeline(steps)
         cv = int(1/validation_size)
-        gridsearch = GridSearchCV(p, hyperparameters, scoring=make_scorer(mean_squared_error), cv=cv, n_jobs=n_process)
+        gridsearch = GridSearchCV(p, hyperparameters, scoring=make_scorer(r2_score), cv=cv, n_jobs=n_process)
         gridsearch.fit(data, ages)
         return_dict["gridsearch_best_estimator"] = gridsearch.best_estimator_
         return_dict["best_cv_score"] = gridsearch.best_score_
@@ -257,10 +282,15 @@ def main(
         best_pipeline = gridsearch.best_estimator_
         best_pipeline.fit(data, ages)
 
+
+
+        y_pred = best_pipeline.predict(data)
+        print("train r^2 was {}".format(r2_score(ages, y_pred)))
+
         y_pred = best_pipeline.predict(testData)
         test_score = mean_squared_error(testAges, y_pred)
         print("test_score: {}".format(test_score))
-        print("r^2 was {}".format(r2_score(testAges, y_pred)))
+        print("test r^2 was {}".format(r2_score(testAges, y_pred)))
         return_dict["test_score"] = test_score
         pkl.dump(return_dict, open("predict_{}Exp.pkl".format(return_mode), 'wb'))
         ex.add_artifact("predict_{}Exp.pkl".format(return_mode))
@@ -280,17 +310,32 @@ def main(
             steps = steps[1:]
         p = Pipeline(steps)
         cv = int(1/validation_size)
-        gridsearch = GridSearchCV(p, hyperparameters, scoring=make_scorer(f1_score, average="weighted"), cv=cv, n_jobs=n_process)
+        gridsearch = GridSearchCV(p, hyperparameters, scoring=make_scorer(r2_score), cv=cv, n_jobs=n_process)
         gridsearch.fit(data, ages)
         return_dict["gridsearch_best_estimator"] = gridsearch.best_estimator_
         return_dict["best_cv_score"] = gridsearch.best_score_
         print("best cv score was {}".format(gridsearch.best_score_))
         best_pipeline = gridsearch.best_estimator_
         best_pipeline.fit(data, ages)
+        y_pred = best_pipeline.predict(data)
+        print("train r^2 was {}".format(r2_score(ages, y_pred)))
+
 
         y_pred = best_pipeline.predict(testData)
         test_score = f1_score(testAges, y_pred, average="weighted")
-        print("test_score: {}".format(test_score))
+
+
+        y_pred_orig = kbins.inverse_transform(y_pred.reshape(-1, 1))
+        test_ages_orig = kbins.inverse_transform(testAges.reshape(-1, 1))
+
+        print("test r^2 was {}".format(r2_score(testAges, y_pred)))
+        print("test mse was {}".format(mean_squared_error(test_ages_orig, y_pred_orig)))
+
+
+
+        print("test_score: f1 {}".format(test_score))
+        print("test_score: accuracy {}".format(accuracy_score(testAges, y_pred)))
+
         return_dict["test_score"] = test_score
         pkl.dump(return_dict, open("predict_{}Exp.pkl".format(return_mode), 'wb'))
         ex.add_artifact("predict_{}Exp.pkl".format(return_mode))
