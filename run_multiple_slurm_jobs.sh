4b5a79cadaa32aab59f7e54de02895ee788c39a4
Mohammed Saqib <saqibm128@gmail.com>
2019-07-17 09:38:09 -0400

    first pass at data ensembler

--- run_multiple_slurm_jobs.sh
+++ run_multiple_slurm_jobs.sh
@@ -1,16 +1,34 @@
 #!/usr/bin/env bash
 
-for num_conv_spatial_layers in 1 3 5
+# for num_conv_spatial_layers in 1 3 5
+# do
+#     for num_conv_temporal_layers in 1 3 5
+#     do
+#       for conv_spatial_filter in "conv_spatial_filter_2_2" "conv_spatial_filter_3_3"
+#       do
+#           for conv_temporal_filter in "conv_temporal_filter_1_7" "conv_temporal_filter_2_3"
+#             do
+#               for num_spatial_filter in 100 200 300
+#               do
+#                 sbatch -n 1 --mem-per-cpu 16G -t 4:00:00 -p gpu --gres=gpu:1 run.sh model_name=$(hexdump -n 16 -e '4/4 "%08X" 1 "\n"' /dev/urandom).h5 n_process=6 use_vp=False num_conv_spatial_layers=$num_conv_spatial_layers num_conv_temporal_layers=$num_conv_temporal_layers $conv_spatial_filter num_spatial_filter=$num_spatial_filter $conv_temporal_filter
+#               done
+#         done
+#       done
+#     done
+# done
+
+
+for num_conv_spatial_layers in 7
 do
-    for num_conv_temporal_layers in 1 3 5
+    for num_conv_temporal_layers in 7
     do
       for conv_spatial_filter in "conv_spatial_filter_2_2" "conv_spatial_filter_3_3"
       do
           for conv_temporal_filter in "conv_temporal_filter_1_7" "conv_temporal_filter_2_3"
             do
-              for num_spatial_filter in 100 200 300
+              for num_spatial_filter in 200
               do
-                sbatch -n 1 --mem-per-cpu 16G -t 4:00:00 -p gpu --gres=gpu:1 run.sh model_name=$(hexdump -n 16 -e '4/4 "%08X" 1 "\n"' /dev/urandom).h5 n_process=6 use_vp=False num_conv_spatial_layers=$num_conv_spatial_layers num_conv_temporal_layers=$num_conv_temporal_layers $conv_spatial_filter num_spatial_filter=$num_spatial_filter $conv_temporal_filter
+                sbatch -n 1 --mem-per-cpu 32G -t 6:00:00 -p gpu --gres=gpu:1 run.sh model_name=$(hexdump -n 16 -e '4/4 "%08X" 1 "\n"' /dev/urandom).h5 n_process=1 lr=0.00005 use_vp=False num_conv_spatial_layers=$num_conv_spatial_layers num_conv_temporal_layers=$num_conv_temporal_layers $conv_spatial_filter num_spatial_filter=$num_spatial_filter $conv_temporal_filter
               done
         done
       done
