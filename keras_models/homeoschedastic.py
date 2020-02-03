f75be200d6f45e350a746c2dca459a69c25ad1a8
Mohammed Saqib <saqibm128@gmail.com>
2020-02-02 23:23:54 -0500

    fixed bug with adversarial weights too

--- keras_models/homeoschedastic.py
+++ keras_models/homeoschedastic.py
@@ -37,7 +37,7 @@ class HomeoschedasticMultiLossLayer(Layer):
         for i, zipped_args in enumerate(zip(ys_true, ys_pred, self.log_vars)):
             y_true, y_pred, log_var = zipped_args
             precision = K.exp(-log_var[0])
-            loss += K.sum(precision * self.multiplier[i] * self.loss_funcs[i](y_true, y_pred)**2. + log_var[0], -1)
+            loss += K.sum(precision * self.multiplier[i] * self.loss_funcs[i](y_true, y_pred)**2. + self.multiplier[i] * log_var[0], -1)
         return K.mean(loss)
 
     def call(self, inputs):
