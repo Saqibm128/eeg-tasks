6a476e22efcdc4459256c6282b0c801a8c067295
Mohammed Saqib <saqibm128@gmail.com>
2020-02-05 00:24:16 -0500

    latest changes

--- predictSeizureMultipleLabels.py
+++ predictSeizureMultipleLabels.py
@@ -46,8 +46,8 @@ from keras_models.homeoschedastic import HomeoschedasticMultiLossLayer
 from keras.losses import categorical_crossentropy, binary_crossentropy
 
 from addict import Dict
-ex = sacred.Experiment(name="homeo_seizure_conv_exp_domain_adapt_v5")
-#
+ex = sacred.Experiment(name="seizure_conv_exp_domain_adapt_v5")
+
 ex.observers.append(MongoObserver.create(client=util_funcs.get_mongo_client()))
 
 # https://pynative.com/python-generate-random-string/
